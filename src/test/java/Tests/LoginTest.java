de332134a2397afd51dee5a6c3e339e1f3713f55
Abdelmagid <abdelmagid.musa@mumzworld.com>
2024-05-22 21:53:53 +0400

    Fixed some issue

--- src/test/java/Tests/LoginTest.java
+++ src/test/java/Tests/LoginTest.java
@@ -1,23 +1,28 @@
 package Tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.Login;
 
 public class LoginTest extends BasePage{
-
+    Login mylogin;
     public String name = "will";
     public String pass = "will";
     public String invalidPass = "jjjjjdlfknmelfjek";
 
     @Test
-    public void verify_valid_login() throws InterruptedException {
-        Login login = new Login(driver);
-        login.login_operation(name, pass);
+    public void verify_login_with_valid_username_and_password() throws InterruptedException {
+        mylogin = new Login(driver);
+        mylogin.login_operation(name, pass);
+        mylogin.is_home_page_displayed();
+        String actual_url = driver.getCurrentUrl();
+        String excepted_url = "https://suite8demo.suiteondemand.com/#/home";
+        Assert.assertEquals( actual_url, excepted_url);
     }
 
     @Test
-    public void verify_valid_name_invalid_password_login() throws InterruptedException {
-        Login login = new Login(driver);
-        login.login_operation(name, invalidPass);
+    public void verify_login_with_invalid_username_and_password() throws InterruptedException {
+        mylogin = new Login(driver);
+        mylogin.login_operation(name, invalidPass);
     }
 }
--- src/test/java/pages/Login.java
+++ src/test/java/pages/Login.java
@@ -3,26 +3,38 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class Login {
     WebDriver driver;
+    Wait<WebDriver> wait;
 
 
     By username = By.name("username");
     By password = By.name("password");
-    By loginBtn = By.name("username");
+    By loginBtn = By.id("login-button");
+    By homePageLocator = By.id("tab0");
 
     public Login(WebDriver driver){
         this.driver = driver;
     }
 
     public void login_operation(String name, String pass) throws InterruptedException {
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(d -> driver.findElement(username).isDisplayed());
         driver.findElement(username).clear();
         driver.findElement(username).sendKeys(name);
         driver.findElement(password).clear();
         driver.findElement(password).sendKeys(pass);
         driver.findElement(loginBtn).click();
-        Thread.sleep(5000);
+    }
+
+    public boolean is_home_page_displayed(){
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+       return wait.until(d -> driver.findElement(homePageLocator).isDisplayed());
     }
 
 
